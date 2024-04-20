254d414f882ba4c1721178403e7f26cb2d12f6f7
D-I-V-Y-A-S <divyaselvam1405@gmail.com>
2024-04-20 20:24:13 +0530

    media

--- middleware/multer.js
+++ middleware/multer.js
@@ -15,11 +15,10 @@ const storage = multer.diskStorage({
                 console.log(file)
                 // cb(null, Date.now() +path.extname(file.originalname));[thrash value num will be file name ]
                 cb(null,file.originalname)
-                //if null not given it considers it as a error.once 2 arg is completed its process through null we send process is successful to router then it calls getImage func.
-                //original file name will be saved
+                
             }
             else {
-                cb("file already exists")//if null is given getImage is called and image uploaded is displayed.
+                cb("file already exists")
             }
         }
         )
